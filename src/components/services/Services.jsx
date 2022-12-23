782c8c57cb95095b1451c3ce4a791a28fa38cf85
Sagar Gupta <sg85207@gmail.com>
2022-12-23 23:12:30 +0530

    Service Section
    Service Section Design and Ui part done still content correction remaining.

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,9 +1,97 @@
 import React from 'react'
 import './services.css'
+import {GiCheckMark} from 'react-icons/gi'
+
 const services = () => {
   return (
     <section id='services'>
-      Services
+      <h5>What I Offer</h5>
+      <h2>Services</h2>
+      <div className="container services__container">
+        <article className="service">
+          <div className="service__head">
+            <h3>UI Design</h3>
+          </div>
+          <ul className="service__list">
+            <li>
+              <GiCheckMark className='service__list-icon' />
+              <p>Web design</p>
+            </li>
+            <li>
+              <GiCheckMark className='service__list-icon' />
+              <p>Responsive design</p>
+            </li>
+            <li>
+              <GiCheckMark className='service__list-icon' />
+              <p>Fast & Easy Navigation</p>
+            </li>
+            <li>
+              <GiCheckMark className='service__list-icon' />
+              <p>Modern UI design</p>
+            </li>
+            <li>
+              <GiCheckMark className='service__list-icon' />
+              <p>Animation effects</p>
+            </li>
+          </ul>
+        </article>
+
+        <article className="service">
+          <div className="service__head">
+            <h3>Web Development</h3>
+          </div>
+          <ul className="service__list">
+            <li>
+              <GiCheckMark className='service__list-icon' />
+              <p>Web design</p>
+            </li>
+            <li>
+              <GiCheckMark className='service__list-icon' />
+              <p>Responsive design</p>
+            </li>
+            <li>
+              <GiCheckMark className='service__list-icon' />
+              <p>Fast & Easy Navigation</p>
+            </li>
+            <li>
+              <GiCheckMark className='service__list-icon' />
+              <p>Modern UI design</p>
+            </li>
+            <li>
+              <GiCheckMark className='service__list-icon' />
+              <p>Animation effects</p>
+            </li>
+          </ul>
+        </article>
+
+        <article className="service">
+          <div className="service__head">
+            <h3>Problem Solving</h3>
+          </div>
+          <ul className="service__list">
+            <li>
+              <GiCheckMark className='service__list-icon' />
+              <p>Web design</p>
+            </li>
+            <li>
+              <GiCheckMark className='service__list-icon' />
+              <p>Responsive design</p>
+            </li>
+            <li>
+              <GiCheckMark className='service__list-icon' />
+              <p>Fast & Easy Navigation</p>
+            </li>
+            <li>
+              <GiCheckMark className='service__list-icon' />
+              <p>Modern UI design</p>
+            </li>
+            <li>
+              <GiCheckMark className='service__list-icon' />
+              <p>Animation effects</p>
+            </li>
+          </ul>
+        </article>
+      </div>
     </section>
   )
 }
