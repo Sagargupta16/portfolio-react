79a86e2926c557e4ef8107b25d0ea0420e1f080e
Sagar Gupta <sg85207@gmail.com>
2022-12-27 11:42:30 +0530

    Contact,footer done
    Done!

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,10 +1,56 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import './contact.css'
+import {MdOutlineEmail} from 'react-icons/md'
+import {ImWhatsapp} from 'react-icons/im'
+import {IoCallOutline} from 'react-icons/io5'
+import emailjs from 'emailjs-com';
 
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_lyt547p', 'template_yz438w6', form.current, 'PAcL61ygLI8WYG16R')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+    e.target.reset();
+  };
   return (
     <section id='contact'>
-      Contact
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className="contact__option">
+            <MdOutlineEmail className="contact__icon" />
+            <h4>Email</h4>
+            <h5>sg85207@gmail.com</h5>
+            <a href="mailto:sg85207@gmail.com" target="_blank" rel="noreferrer">Send a Message</a>
+          </article>
+          <article className="contact__option">
+            <ImWhatsapp className="contact__icon" />
+            <h4>WhatsApp</h4>
+            <h5>+91-8770532413</h5>
+            <a href="https://wa.me/+918770532413" target="_blank" rel="noreferrer">Send a Message</a>
+          </article>
+          <article className="contact__option">
+            <IoCallOutline className="contact__icon" />
+            <h4>Call me</h4>
+            <h5>+91-8770532413</h5>
+            <a href="tel:+918770532413" target="_blank" rel="noreferrer">Call</a>
+          </article>
+        </div>
+        <form ref={form} onSubmit={sendEmail} className="contact__form">
+          <input type="text" name='name' placeholder='Your Full Name' required />
+          <input type="email" name='email' placeholder='Your Email' required />
+          <textarea name="message" rows="7" placeholder='Your Message' required></textarea>
+          <button type='submit' className='btn btn-primary'>Send Message</button>
+        </form>
+      </div>
     </section>
   )
 }
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,10 +1,35 @@
+/* eslint-disable jsx-a11y/anchor-has-content */
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react'
 import './footer.css'
+import {FaInstagram, FaLinkedin, FaGithub ,FaTwitter} from 'react-icons/fa'
+
 const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
+    <footer>
+      <a href="#" className='footer__logo'>SAGAR</a>
+      <ul className="permalinks">
+        <li><a href="#">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#education">Education</a></li>
+        <li><a href="#skill">Skills</a></li>
+        <li><a href="#services">Services</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#testimonial">Testimonials</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+      <div className="footer__socials">
+        <a href="https://www.instagram.com/sagar_seth16/" target="_blank" rel="noreferrer"><FaInstagram/></a>
+        <a href="https://twitter.com/SagarGupta1610" target="_blank" rel="noreferrer"><FaTwitter/></a>
+        <a href="https://www.linkedin.com/in/sagar-gupta-16-10/" target="_blank" rel="noreferrer"><FaLinkedin/></a>
+        <a href="https://github.com/Sagargupta16" target="_blank" rel="noreferrer"><FaGithub/></a>
+      </div>
+
+      <div className="footer__copyright">
+        <small>© Sagar Gupta. All rights reserved.</small>
+      </div>
+
+    </footer>
   )
 }
 
