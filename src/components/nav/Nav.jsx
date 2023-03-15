9820eb8961018d8b963063410e64f7b14f160fb1
Sagar Gupta <sg85207@gmail.com>
2023-03-15 21:36:44 +0530

    Nav Mobile View WIP

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -14,16 +14,23 @@ const Nav = () => {
     window.scrollTo(0, 0);
   });
   return (
-    <nav >
-      <Link to="/" className={activeNav === '#' ? 'active' : ''} onClick={() => setActiveNav('#')} alt="Home"><RiHome3Line/></Link>
-      <Link to="/about" className={activeNav === 'about' ? 'active' : ''} onClick={() => setActiveNav('about')} alt="About"><AiOutlineUser/></Link>
-      <Link to="/education" className={activeNav === 'education' ? 'active' : ''} onClick={() => setActiveNav('education')} alt="Education"><GoPencil/></Link>
-      <Link to="/skill" className={activeNav === 'skill' ? 'active' : ''} onClick={() => setActiveNav('skill')} alt="Skills"><GiSkills/></Link>
-      <Link to="/services" className={activeNav === 'services' ? 'active' : ''} onClick={() => setActiveNav('services')} alt="Services"><RiServiceLine/></Link>
-      <Link to="/portfolio" className={activeNav === 'portfolio' ? 'active' : ''} onClick={() => setActiveNav('portfolio')} alt="Portfolio"><GiSuitcase/></Link>
-      <Link to="/testimonial" className={activeNav === 'testimonial' ? 'active' : ''} onClick={() => setActiveNav('testimonial')} alt="Testimonial"><GiChatBubble/></Link>
-      <Link to="/contact" className={activeNav === 'contact' ? 'active' : ''} onClick={() => setActiveNav('contact')} alt="Contact"><AiOutlineMessage/></Link>
-    </nav>
+    <>
+      <button className='nav-btn' onClick={() => document.querySelector('.nav').classList.toggle('active')}>
+        <span className='nav-btn__line'></span>
+        <span className='nav-btn__line'></span>
+        <span className='nav-btn__line'></span>
+      </button>
+      <nav className='nav'>
+        <Link to="/" className={activeNav === '#' ? 'active' : ''} onClick={() => setActiveNav('#')} alt="Home"><RiHome3Line/></Link>
+        <Link to="/about" className={activeNav === 'about' ? 'active' : ''} onClick={() => setActiveNav('about')} alt="About"><AiOutlineUser/></Link>
+        <Link to="/education" className={activeNav === 'education' ? 'active' : ''} onClick={() => setActiveNav('education')} alt="Education"><GoPencil/></Link>
+        <Link to="/skill" className={activeNav === 'skill' ? 'active' : ''} onClick={() => setActiveNav('skill')} alt="Skills"><GiSkills/></Link>
+        <Link to="/services" className={activeNav === 'services' ? 'active' : ''} onClick={() => setActiveNav('services')} alt="Services"><RiServiceLine/></Link>
+        <Link to="/portfolio" className={activeNav === 'portfolio' ? 'active' : ''} onClick={() => setActiveNav('portfolio')} alt="Portfolio"><GiSuitcase/></Link>
+        <Link to="/testimonial" className={activeNav === 'testimonial' ? 'active' : ''} onClick={() => setActiveNav('testimonial')} alt="Testimonial"><GiChatBubble/></Link>
+        <Link to="/contact" className={activeNav === 'contact' ? 'active' : ''} onClick={() => setActiveNav('contact')} alt="Contact"><AiOutlineMessage/></Link>
+      </nav>
+    </>
   )
 }
 
