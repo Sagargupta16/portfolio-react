6f4354309260ddf31038180901742ed0f3950652
Sagar Gupta <sg85207@gmail.com>
2023-07-30 23:33:43 +0530

    Jsx Removed From Education File

--- src/components/education/Education.jsx
+++ src/components/education/Education.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./education.css";
-import EducationItem from "./EducationItem.jsx";
+import EducationItem from "./EducationItem";
 
 const EducationArray = [
   {
