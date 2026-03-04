acd1a815c55613a4012fab6a3ecfe5f2cac927a3
Sagargupta16 <sg85207@gmail.com>
2026-03-04 22:18:30 +0530

    feat: add project data and configure custom domain deployment.

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig(({ mode }) => ({
       jsxRuntime: 'automatic'
     })
   ],
-  base: mode === 'production' ? '/portfolio-react/' : '/',
+  base: '/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
